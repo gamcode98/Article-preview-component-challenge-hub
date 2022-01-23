b0d91e090d14662b690bb9aa5da14f654934a01f
Gabriel <gamcode98@gmail.com>
2022-01-22 22:39:19 -0300

    a fix

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const d = document;
+const $btnShare = d.getElementById("btn-share");
+const $shareSocialMedia = d.querySelector(".share-social-media");
+const $path = d.querySelector("path");
+// $btnShare.addEventListener("click", () => {
+//   $shareSocialMedia.classList.toggle("share-social-media--show");
+//   $btnShare.classList.toggle("main-personal-btn-is-active");
+//   $path.classList.toggle("share-icon-path");
+// });
