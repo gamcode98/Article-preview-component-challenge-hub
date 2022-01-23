677add84fe40ca8e704b5c1531c16a1293c20163
Gabriel <gamcode98@gmail.com>
2022-01-23 10:05:20 -0300

    fixed

--- js/index.js
+++ js/index.js
@@ -7,3 +7,15 @@ $btnShare.addEventListener("click", () => {
   $btnShare.classList.toggle("main-personal-btn-is-active");
   $path.classList.toggle("share-icon-path");
 });
+
+window.addEventListener("resize", () => {
+  if (
+    $shareSocialMedia.classList.contains("share-social-media--show") &&
+    $btnShare.classList.contains("main-personal-btn-is-active") &&
+    $path.classList.contains("share-icon-path")
+  ) {
+    $shareSocialMedia.classList.remove("share-social-media--show");
+    $btnShare.classList.remove("main-personal-btn-is-active");
+    $path.classList.remove("share-icon-path");
+  }
+});
